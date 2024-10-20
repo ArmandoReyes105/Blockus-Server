b5f91e735cc89f7b764cecb845b3b8acda2d6491
ArmandoReyes105 <arrey105@outlook.com>
2024-10-20 15:12:58 -0600

    Configuración inicial del proyecto

---
+++ Services/Implementations/ServiceImplementation.cs
@@ -0,0 +1,18 @@
+﻿using Services.Interfaces;
+using Data;
+using Data.Model; 
+
+namespace Services.Implementations
+{
+    public class ServiceImplementation : IService1
+    {
+        public string GetData(int value)
+        {
+
+            Class1 class1 = new Class1();
+            Account account = new Account(); 
+
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
