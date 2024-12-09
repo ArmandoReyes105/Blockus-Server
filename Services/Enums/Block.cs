1b51f0452c13f1791a945dfee96e67ed26e07061
x3776x <abrahamvvquinto@gmail.com>
2024-12-09 00:40:13 -0600

    added the 21 pieces to Enum and BlockList.

--- Services/Enums/Block.cs
+++ Services/Enums/Block.cs
@@ -12,6 +12,60 @@ namespace Services.Enums
         Block02,
 
         [EnumMember]
-        Block03
+        Block03,
+
+        [EnumMember]
+        Block04,
+
+        [EnumMember]
+        Block05,
+
+        [EnumMember]
+        Block06,
+
+        [EnumMember]
+        Block07,
+
+        [EnumMember]
+        Block08,
+
+        [EnumMember]
+        Block09,
+
+        [EnumMember]
+        Block10,
+
+        [EnumMember]
+        Block11,
+
+        [EnumMember]
+        Block12,
+
+        [EnumMember]
+        Block13,
+
+        [EnumMember]
+        Block14,
+
+        [EnumMember]
+        Block15,
+
+        [EnumMember]
+        Block16,
+
+        [EnumMember]
+        Block17,
+
+        [EnumMember]
+        Block18,
+
+        [EnumMember]
+        Block19,
+
+        [EnumMember]
+        Block20,
+
+        [EnumMember]
+        Block21,
     }
 }
